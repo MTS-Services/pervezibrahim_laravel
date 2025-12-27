dc674cc810c8650653344da5a1bc469f2ca4e7f4
Rashed Miya <132655219+rashedsmiya@users.noreply.github.com>
2025-12-27 16:39:15 +0600

    add

--- resources/views/livewire/frontend/home.blade.php
+++ resources/views/livewire/frontend/home.blade.php
@@ -1,15 +1,42 @@
 <div>
-    <section class="h-[80vh] bg-bg-primary text-center justify-center items-center flex w-full mx-auto">
-        <h1 class="text-5xl font-bold text-second-500">This is Home page </h1>
+    <section class="rounded-3xl relative bg-black text-white min-h-[50vh] overflow-hidden ml-92 mr-92">
+        <!-- Background Pattern Overlay -->
+        <div class="absolute inset-0 opacity-40 pointer-events-none">
+            <img src="{{ asset('assets/images/home_page/background_images.png') }}" alt=""
+                class="w-full h-full object-none" />
+        </div>
 
-        <x-ui.button type="submit" class="w-auto! py-2!" >
-            {{ __('Sign in') }}
-        </x-ui.button>
-        <x-ui.button type="submit" class="w-auto! py-2!" variant="orange-tertiary">
-            {{ __('Sign in') }}
-        </x-ui.button>
-        <x-ui.button type="submit" class="w-auto! py-2!" variant="secondary">
-            {{ __('Sign in') }}
-        </x-ui.button>
+        <!-- Header / Navigation -->
+        <header class="relative z-10 flex items-center justify-between px-6 lg:px-16 py-8">
+
+
+        </header>
+
+        <!-- Hero Content -->
+        <main class="relative z-10 container mx-auto px-6 lg:px-16 mt-12 grid lg:grid-cols-2 gap-12 items-center">
+            <!-- Left: Video Player Section -->
+            <div class="relative group w-[500px] h-full">
+                <!-- Video Container -->
+                <div class="relative overflow-hidden ">
+                    <img src="{{ asset('assets/images/home_page/Rectangle 31.png') }}" alt="Business Team"
+                        class="w-full h-full object-cover " style="object-position: 15% center;" />
+                    <!-- Video Overlay Content -->
+                </div>
+            </div>
+
+            <!-- Right: Text Section -->
+            <div class="flex flex-col space-y-8 mt-52">
+                <h1 class="text-2xl font-light text-wrap text-white">
+                    Business Process Management System and Methods for the New Era of Technology
+                </h1>
+
+                <div>
+                    <a href="#"
+                        class="inline-block px-10 py-3 rounded-full border border-white/50 text-white font-medium hover:bg-white hover:text-black transition-all">
+                        Learn More
+                    </a>
+                </div>
+            </div>
+        </main>
     </section>
 </div>
