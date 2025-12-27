019bfde36af3340f4fcaf25c6e8426dc31e5ef2c
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2025-12-27 11:13:34 +0600

    update

--- app/Livewire/Frontend/Home.php
+++ app/Livewire/Frontend/Home.php
@@ -3,17 +3,11 @@
 namespace App\Livewire\Frontend;
 
 use Livewire\Component;
-use App\Models\TikTokVideo;
-use App\Services\TikTokService;
-use App\Services\BannerVideoService;
-use App\Services\KeywordService;
-use Illuminate\Support\Facades\Log;
 
 class Home extends Component
 {
-      public function render()
+    public function render()
     {
-        
 
         return view('livewire.frontend.home');
     }
--- resources/views/livewire/frontend/home.blade.php
+++ resources/views/livewire/frontend/home.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <section class="h-screen text-center items-center">
-        <h1>This is Home page </h1>
+    <section class="h-[80vh] text-center justify-center items-center flex w-full mx-auto">
+        <h1 class="text-5xl font-bold">This is Home page </h1>
     </section>
 </div>
