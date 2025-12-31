fed2df49fe8503432fd230c4184a66d1d2cb101b
Md Ahoshan Habib <150496607+ahoshan20@users.noreply.github.com>
2025-12-31 16:05:02 +0600

    FAQ seeder added

--- app/Traits/AuditColumnsTrait.php
+++ app/Traits/AuditColumnsTrait.php
@@ -9,7 +9,7 @@ trait AuditColumnsTrait
     public function addAdminAuditColumns(Blueprint $table): void
     {
         $table->timestamp('restored_at')->nullable();
-        
+
         $table->unsignedBigInteger('created_by')->nullable();
         $table->unsignedBigInteger('updated_by')->nullable();
         $table->unsignedBigInteger('deleted_by')->nullable();
@@ -41,6 +41,43 @@ trait AuditColumnsTrait
         $table->dropColumn('updated_by');
         $table->dropColumn('deleted_by');
         $table->dropColumn('restored_by');
+        $table->dropColumn('restored_at');
+    }
+
+    public function addMorphedAuditColumns(Blueprint $table): void
+    {
+        $table->unsignedBigInteger('creater_id')->nullable();
+        $table->string('creater_type')->nullable();
+        $table->unsignedBigInteger('updater_id')->nullable();
+        $table->string('updater_type')->nullable();
+        $table->unsignedBigInteger('deleter_id')->nullable();
+        $table->string('deleter_type')->nullable();
+        $table->unsignedBigInteger('restorer_id')->nullable();
+        $table->string('restorer_type')->nullable();
+
+        $table->timestamp('restored_at')->nullable();
+
+        $table->index(['creater_id', 'creater_type']);
+        $table->index(['updater_id', 'updater_type']);
+        $table->index(['deleter_id', 'deleter_type']);
+        $table->index(['restorer_id', 'restorer_type']);
+    }
+
+    public function dropMorphedAuditColumns(Blueprint $table): void
+    {
+        $table->dropIndex(['creater_id', 'creater_type']);
+        $table->dropIndex(['updater_id', 'updater_type']);
+        $table->dropIndex(['deleter_id', 'deleter_type']);
+        $table->dropIndex(['restorer_id', 'restorer_type']);
+
+        $table->dropColumn('creater_id');
+        $table->dropColumn('creater_type');
+        $table->dropColumn('updater_id');
+        $table->dropColumn('updater_type');
+        $table->dropColumn('deleter_id');
+        $table->dropColumn('deleter_type');
+        $table->dropColumn('restorer_id');
+        $table->dropColumn('restorer_type');
         $table->dropColumn('restored_at');
     }
 }
