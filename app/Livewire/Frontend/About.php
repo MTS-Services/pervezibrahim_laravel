9c2514abe5158210ab1c19b270cc388fea0a158e
Md Ahoshan Habib <150496607+ahoshan20@users.noreply.github.com>
2026-01-01 17:06:54 +0600

    Updated

--- app/Livewire/Frontend/About.php
+++ app/Livewire/Frontend/About.php
@@ -2,12 +2,47 @@
 
 namespace App\Livewire\Frontend;
 
+use App\Enums\ActiveInactive;
+use App\Enums\Page;
+use App\Services\VideoService;
 use Livewire\Component;
 
 class About extends Component
 {
+    protected VideoService $videoService;
+
+    public function boot(VideoService $videoService)
+    {
+        $this->videoService = $videoService;
+    }
     public function render()
     {
-        return view('livewire.frontend.about');
+        $aboutVideos = $this->videoService->getPaginatedData(
+            perPage: 2,
+            filters: $this->getAboutFilters()
+        );
+        $galleryVideos = $this->videoService->getPaginatedData(
+            perPage: 6,
+            filters: $this->getGalleryFilters()
+        );
+        return view('livewire.frontend.about',[
+            'aboutVideos' => $aboutVideos,
+            'galleryVideos' => $galleryVideos,
+        ]);
+    }
+
+    protected function getGalleryFilters(): array
+    {
+        return [
+            'status' => ActiveInactive::ACTIVE->value,
+            'page' => Page::GALLERY->value,
+        ];
+    }
+    protected function getAboutFilters(): array
+    {
+        return [
+            'status' => ActiveInactive::ACTIVE->value,
+            'page' => Page::ABOUT_US->value,
+        ];
     }
 }
